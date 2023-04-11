58c41eb905c202dc85c65f1b3786b018ec3b7d8c
[Ivan Filippov] <ivan.crossfit@gmail.com>
2023-04-11 12:16:35 +0300

    Cam fix

--- Assets/Scripts/Root/RaceSceneHandler.cs
+++ Assets/Scripts/Root/RaceSceneHandler.cs
@@ -47,9 +47,18 @@ namespace RaceManager.Root
             EffectsController.InstallSettings(_settingsContainer);
             StartPlayingRandomRaceTrack();
 
-            this.UpdateAsObservable()
+            this.FixedUpdateAsObservable()
                 .Where(_ => _raceUI.RaceFinished == false)
                 .Subscribe(_ =>
+                {
+                    HandleWheels();
+                    HandleCarSpeed();
+                })
+                .AddTo(this);
+
+            this.UpdateAsObservable()
+                .Where(_ => _raceUI.RaceFinished == false)
+                .Subscribe(_ => 
                 {
                     if (Input.GetMouseButtonDown(0))
                         _gameEvents.ScreenTaped.OnNext();
@@ -59,11 +68,7 @@ namespace RaceManager.Root
 
                     if (Input.GetMouseButtonUp(0))
                         _gameEvents.ScreenTapReleased.OnNext();
-
-                    HandleWheels();
-                    HandleCarSpeed();
-                })
-                .AddTo(this);
+                }).AddTo(this);
 
             EventsHub<RaceEvent>.Subscribe(RaceEvent.START, SendStartNotification);
             _raceUI.OnButtonPressed += PlayButtonPressedEffect;

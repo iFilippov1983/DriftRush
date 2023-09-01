e82184b3af68317ace5839352a3cd4291c366707
[Ivan Filippov] <ivan.crossfit@gmail.com>
2023-09-01 10:38:23 +0300

    Cameras change

--- Assets/Scripts/UI/RaceUI/TutorialContextHandler.cs
+++ Assets/Scripts/UI/RaceUI/TutorialContextHandler.cs
@@ -99,7 +99,6 @@ namespace RaceManager.UI
                 return;
 
             _flagsHandler
-                //.Subscribe(gameFlag, TimeScaleToDefault)
                 .Subscribe(gameFlag, () => TimeScaleTo(timeScaleValue))
                 .AddTo(this);
         }
